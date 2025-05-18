f0e2be26374997e198bcb23e557af2429e2f8bcd
T6113 <thomishamyers@gmail.com>
2025-05-18 11:53:24 -0500

    some change

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ addTodoForm.addEventListener("submit", (evt) => {
 });
 
 initialTodos.forEach((item) => {
-  renderTodo(item);
+  
 });
 
 const todoValidator = new FormValidator(validationConfig, addTodoForm);
